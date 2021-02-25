28e03031a0b69260750fc0785321f1a3edbeaf01
Melody Miller <mel@taragon.net>
2021-02-25 18:14:29 -0500

    Bumping last calc date to recalculate reports affected by card bonus issue

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] =  os.environ['DATABASE_URL']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-LAST_CALC_DATE = datetime.fromtimestamp(1614271279)
+LAST_CALC_DATE = datetime.fromtimestamp(1614294824)
 
 token = get_bearer_token()
 
