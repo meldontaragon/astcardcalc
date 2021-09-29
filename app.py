bc093d59367a89b75827f284b6fad1d2b01fe649
Melody Miller <mel@taragon.net>
2021-09-29 18:05:48 -0400

    Bump to last calc date

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] =  os.environ['DATABASE_URL']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-LAST_CALC_DATE = datetime.fromtimestamp(1620521812)
+LAST_CALC_DATE = datetime.fromtimestamp(1632953126)
 
 token = get_bearer_token()
 
